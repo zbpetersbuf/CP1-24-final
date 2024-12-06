839cb44fcdd70aadef4948e98e73888a3d5737aa
zbpetersbuf <zbpeters@buffalo.edu>
2024-12-06 20:55:39 +0000

    message

--- zbpetersbuf/code/final.py
+++ zbpetersbuf/code/final.py
@@ -11,13 +11,15 @@ import numpy as np
 import workinh as wrk
 from scipy.optimize import curve_fit
 
-def fah_to_kel(f):
-    """Converts from Fahrenheit to Kelvin enter the recorded temp as f"""
 
-    if f < -459.66:
-        print("The value for Fahrenheit should not be possible try again")
-        return None
-    return (5*(f-32))/9 + 273.15
+def filenamelister(exp_name):
+    """this function finds and returns all markdown files of an experiment type
+    ie if you wanted to find all .md files that relate to the total elevator
+    movment experiment enter 'eletot' for exp_name"""
+
+    pattern = os.path.join('/workspaces/CP1-24-final/zbpetersbuf/data/', f"*{exp_name.strip()}*.md")
+    md_files = glob.glob(pattern)
+    return md_files
 
 def find_fah(file_name):
     """this function finds the temp recorded for the file you enter in file_name
@@ -29,14 +31,13 @@ def find_fah(file_name):
         numbs = re.findall(r'\d+', f.read())
     return int(numbs[0])
 
-def filenamelister(exp_name):
-    """this function finds and returns all markdown files of an experiment type
-    ie if you wanted to find all .md files that relate to the total elevator
-    movment experiment enter 'eletot' for exp_name"""
+def fah_to_kel(f):
+    """Converts from Fahrenheit to Kelvin enter the recorded temp as f"""
 
-    pattern = os.path.join('/workspaces/CP1-24-final/zbpetersbuf/data/', f"*{exp_name.strip()}*.md")
-    md_files = glob.glob(pattern)
-    return md_files
+    if f < -459.66:
+        print("The value for Fahrenheit should not be possible try again")
+        return None
+    return (5*(f-32))/9 + 273.15
 
 def sinfunk(x, a, b, c, d):
     """this is just the fit function for the fit stuff"""
@@ -48,6 +49,8 @@ def fitsincuve(xax,yax):
     sin_fit = sinfunk(np.array(xax), a, b, c, d)
     isfft = np.fft.fft(sin_fit)
     return isfft
+
+
 
 def stepnumb(datta):
     """this just shortens the length of the data to the closest 2^n"""
@@ -65,6 +68,12 @@ def stepnumb(datta):
     remv=(len(tim)-(2**(n-1)))/2
     # i have to modul this to remove and add a half, ie no half. integers, remove one more from begining then end
     return xax[remv:-remv], yax[remv:-remv], tim[remv:-remv]
+
+def f1(file_name,i=0):
+    """file_name to datta"""
+    while i<20:
+        stepnumb(file_name[i])
+    return 
 
 def adjs_Rsqr(yax,y_pred):
     """computs the adjusted r^2 value"""
