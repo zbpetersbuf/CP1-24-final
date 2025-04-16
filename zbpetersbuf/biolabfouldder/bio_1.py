c253d7feffa077adc484fd16700c2075fe85aabd
zbpetersbuf <zbpeters@buffalo.edu>
2025-04-16 22:51:13 +0000

    c

--- zbpetersbuf/biolabfouldder/bio_1.py
+++ zbpetersbuf/biolabfouldder/bio_1.py
@@ -68,4 +68,5 @@ def process_multiple_files(exp_name):
         except Exception as e:
             print(f"Error processing file {file}: {e}")
     #results[file_key] = {'mean': tot_mean/20, 'std_dev': tot_std_dev/20}
-    print(f"Mean: {tot_mean / 20}, Std Dev: {tot_std_dev / 20}")
+    #print(f"Mean: {tot_mean / 20}, Std Dev: {tot_std_dev / 20}")
+    return 0
