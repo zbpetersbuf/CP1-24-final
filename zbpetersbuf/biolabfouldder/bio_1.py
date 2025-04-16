c3d28c0ceec2b14cce2aeb1344f0c550dc186f20
zbpetersbuf <zbpeters@buffalo.edu>
2025-04-16 23:33:12 +0000

    c

--- zbpetersbuf/biolabfouldder/bio_1.py
+++ zbpetersbuf/biolabfouldder/bio_1.py
@@ -7,6 +7,7 @@ import glob
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 #import workinh as wrk
 
 def filenamelister(exp_name, filetype = '.md'):
