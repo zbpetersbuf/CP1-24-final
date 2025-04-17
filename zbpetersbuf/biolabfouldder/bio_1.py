89d575989ca22acf7079995d2e1c730c38d19451
zbpetersbuf <zbpeters@buffalo.edu>
2025-04-17 01:43:25 +0000

    c

--- zbpetersbuf/biolabfouldder/bio_1.py
+++ zbpetersbuf/biolabfouldder/bio_1.py
@@ -134,7 +134,7 @@ def fcs():
     x = df['Time']  # Time column (x-values)
     y = df['Count Rate Channel 1 [kCounts/s]']  # Count Rate Channel 1 [kCounts/s] (y-values)
 
-    x = x/1000
+    x = [a/1000 for a in x]
 
     # Perform correlation calculation
     correlation = np.correlate(y, x, mode='full')  # Auto-correlation of y
