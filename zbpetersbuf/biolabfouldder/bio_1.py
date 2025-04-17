919abc2c1dd56b60557e5751dcd7c06c0bff9105
zbpetersbuf <zbpeters@buffalo.edu>
2025-04-17 16:52:38 +0000

    c

--- zbpetersbuf/biolabfouldder/bio_1.py
+++ zbpetersbuf/biolabfouldder/bio_1.py
@@ -138,7 +138,7 @@ def fcs():
 
     lag = np.arange(-len(x) + 1, len(x))
 
-    adv_correlation = correlation.mean()  # Maximum correlation for normalization
+    adv_correlation = correlation.max()  # Maximum correlation for normalization
     correlation = correlation / adv_correlation
 
     mask = lag > 1200  # Mask for lag > 1
